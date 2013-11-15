b4282f5f21741a73f75c1c2d3495d114ee38f8bf
Tamazy <tamazy@laposte.net>
2013-11-15 11:45:20 +0100

    Final Forme

--- src/BioInfo/BioInfo.java
+++ src/BioInfo/BioInfo.java
@@ -4,7 +4,9 @@ public class BioInfo {
 
 	public static void main(String[] args) {
 		
-		String string = "== BioInfo ==\n\n";
+		String string = "\n= = =     B I O   I N F O R M A T I Q U E     = = =\n\n\n";
+		
+		string += "-- II. MODELISATION PAR PSWM --\n\n\n";
 		
 		Family family = FamilyFactory.makeFamily("Dtrain");
 		string += "Famille :\n" + family.toString() + "\n";
@@ -37,7 +39,6 @@ public class BioInfo {
 		
 		family.calculateNFrequency();
 		//string += family.displayNFrequency() + "\n";
-		//string += family.NFrequency.toString();
 		string += "Frequence null en '-' : " + family.getNFrequency('-') + "\n\n";
 		
 		
@@ -46,13 +47,18 @@ public class BioInfo {
 		string += protein.toString() + "\n\n";
 		
 		
-		string += "Log-vraisemblance i=0 : ";
-		string += family.getLogOddsRatio(protein, 0) + "\n\n";
+		//string += "-> minilogue : " + family.getLogOddsRatio(protein, 66) + "\n\n";
 		
+		string += "Log-vraisemblance  : ";
+		string += family.getLogOddsRatio(protein, 1) + "\n\n";
 
 		string += "Generation de la courbe des logs vraisemblances ... ";
 		family.generateCurveLOR(protein);
 		string += "done!\n\n";
+		
+		
+
+		string += "\n\n-- III. CO-EVOLUTION DE RESIDUES EN CONTACT --\n\n\n";
 		
 		
 		string += "Calcule des occurences doubles ... ";
@@ -60,8 +66,7 @@ public class BioInfo {
 		family.calculateNum2();
 		string += "done !\n";
 		
-		string += family.getNumber2('A',38,'-',43) + "\n";
-		string += family.getNumber2('-',43,'A',38) + "\n";
+		string += "occurences A,38,-,43 : " + family.getNumber2('A',38,'-',43) + "\n";
 		string += "\n";
 		
 		
@@ -70,27 +75,45 @@ public class BioInfo {
 		family.calculateWeights2();
 		string += "done !\n";
 		
-		string += family.getWeight2('A',38,'-',43) + "\n";
-		string += family.getWeight2('-',43,'A',38) + "\n";
+		string += "poids A,38,-,43 : " + family.getWeight2('A',38,'-',43) + "\n";
 		string += "\n";
 		
-		
-		//string += family.getAATypes().size() + "\n";
-		//string += family.getL() + "\n";
 		
 		string += "Calcule de la matrice d'info mutuelle ... ";
 		string = BioInfo.afficher(string);
 		family.calculateMutualInfo();
 		string += "done !\n";
 		
-		string += family.getMutualInfo(0,1) + "\n";
-		string += family.getMutualInfo(1,0) + "\n";
+		string += "position 0,1 : " + family.getMutualInfo(0,1) + "\n";
+		string += "position 21,42 : " + family.getMutualInfo(21,42) + "\n";
 		string += "\n";
 		
 		
+		string += "Trie de la matrice d'info mutuelle ... ";
+		string = BioInfo.afficher(string);
+		family.calculateSortMutualInfo();
+		string += "done !\n\n";
 		
-		/*int i = 49;
-		string += family.getWeight(protein.getAA(i), i);*/
+		
+		string += "Calcule des distances ... ";
+		string = BioInfo.afficher(string);
+		family.calculateDistances();
+		string += "done !\n\n";
+		
+		//string += family.displaySortMutualInfo();
+		
+		string += "Calcule des fractions ... ";
+		string = BioInfo.afficher(string);
+		family.calculateFractions();
+		string += "done !\n\n";
+		
+		
+		string += "Génération de la courbe des fractions ... ";
+		string = BioInfo.afficher(string);
+		family.generateCurveFractions();
+		string += "done !\n\n";
+		
+		
 		System.out.println(string);
 		
 	}
--- src/BioInfo/Family.java
+++ src/BioInfo/Family.java
@@ -20,6 +20,13 @@ public class Family {
 	private HashMap<String, Double> weights2;
 	private HashMap<String, Double> mutualInfo;
 	
+	private ArrayList<String> miSortKey;
+	private ArrayList<Double> miSortValue;
+	private ArrayList<Double> miSortDist;
+	
+	private HashMap<String, Double> distances;
+	
+	private ArrayList<Double> fractions;
 	
 	/* Constructeurs */
 	
@@ -36,7 +43,15 @@ public class Family {
 		
 		this.numbers2 = new HashMap<String,Integer>();
 		this.weights2 = new HashMap<String,Double>();
-		this.mutualInfo = new HashMap<String,Double>(2162,(float) 0.70);
+		this.mutualInfo = new HashMap<String,Double>(10000,(float) 0.70);
+		
+		this.miSortKey = new ArrayList<String>();
+		this.miSortValue = new ArrayList<Double>();
+		this.miSortDist = new ArrayList<Double>();
+		
+		this.distances = new HashMap<String,Double>();
+		
+		this.fractions = new ArrayList<Double>();
 	}
 	
 	/* Getters */
@@ -192,7 +207,7 @@ public class Family {
 		double value;
 		double temp;
 		
-		for(int i=0; i< this.getQ(); i++)
+		for(int i=0; i< this.getL(); i++)
 		{
 			value = log2Q;
 			for(Character AA : this.AATypes)
@@ -206,7 +221,7 @@ public class Family {
 	
 	public String displayREntropy(){
 		String s = "";
-		for(int i=0; i< this.getQ(); i++){
+		for(int i=0; i< this.getL(); i++){
 			s += "[ " + i + ", " + this.REntropy.get(i) + " ]\n";
 		}
 		return s;
@@ -223,9 +238,9 @@ public class Family {
             String fileName = "./data/REntropy.txt";
             PrintWriter out  = new PrintWriter(new FileWriter(fileName));
             
-            for(int i=0; i < this.getQ(); i++)
+            for(int i=0; i < this.getL(); i++)
             {
-            	out.println(i + " " + this.getAA(i) + " " + REntropy.get(i));
+            	out.println(i + " " + REntropy.get(i));
             }
          
             out.close();
@@ -272,16 +287,36 @@ public class Family {
 	
 	/* log-vraisemblance */
 	
+	public double proba(Protein p, int begin)
+	{
+		double rslt = 1.0;
+		for(int i=begin; i < getL() + begin; i++)
+		{
+			System.out.println(p.getAA(i));
+			rslt *= getWeight(p.getAA(i),i-begin);
+		}
+		return rslt;
+	}
+	
+	public double probaNull(Protein p, int begin)
+	{
+		double rslt = 1.0;
+		for(int i=begin; i < getL() + begin; i++)
+		{
+			rslt *= getNFrequency(p.getAA(i));
+		}
+		return rslt;
+	}
 	
 	public double getLogOddsRatio(Protein p, int begin)
 	{
 		double log2 = Math.log(2);
-		double rslt = 0;
+		double rslt = 0.0;
 		
-		for(int i = begin; i < this.getL(); i++)
+		for(int i = begin; i < this.getL() + begin; i++)
 		{
 			rslt += Math.log(	
-						this.getWeight(p.getAA(i), i)	/
+						this.getWeight(p.getAA(i), i-begin)	/
 						this.getNFrequency(p.getAA(i))
 					)	/ log2;
 		}
@@ -291,10 +326,10 @@ public class Family {
 	
 	public ArrayList<Double> getLogOddsRatio(Protein p)
 	{
-		int max = p.length()-getL();
-		ArrayList<Double> logs= new ArrayList<Double>(max);
+		int max = p.length() - getL();
+		ArrayList<Double> logs = new ArrayList<Double>();
 		
-		for(int i=0; i<max; i++)
+		for(int i=0; i <= max; i++)
 		{
 			logs.add(getLogOddsRatio(p,i));
 		}
@@ -334,13 +369,13 @@ public class Family {
 		
 		for(Protein p : proteins)
 		{
-			for(int i=0; i<p.getAAList().length(); i++)
+			for(int i=0; i<p.length(); i++)
 			{	
-				actualAAi = "" + p.getAAList().charAt(i) + i;
+				actualAAi = "" + p.getAA(i) + i;
 				
-				for(int j=i+1; j<p.getAAList().length(); j++)
+				for(int j=i+1; j<p.length(); j++)
 				{
-					actualAAj = "" + p.getAAList().charAt(j) + j;
+					actualAAj = "" + p.getAA(j) + j;
 					
 					actualAA = actualAAi + actualAAj;
 					
@@ -352,8 +387,6 @@ public class Family {
 						actualInt++;
 
 					this.numbers2.put(actualAA, actualInt);
-					
-					//System.out.println(actualAAi + "," + actualAAj + " : " + actualInt);
 				}
 				
 			}
@@ -373,7 +406,9 @@ public class Family {
 	
 	public void calculateWeights2()
 	{
-		int temp;
+		int temp = 0;
+		double temp2 = 0.0;
+		double temp3 = 0.0;
 		
 		for(String AAd : this.numbers2.keySet())
 		{
@@ -381,10 +416,9 @@ public class Family {
 			
 			if(temp != 0) // si =0 on ne stock pas dans la HM, mais on genere avec getWeight2
 			{
-				this.weights2.put(AAd, 
-					(temp + 1/ (double) this.getQ())
-					/
-					(double) (this.getM() + this.getQ()));
+				temp2 = (1.0 / (double) this.getQ());
+				temp3 = (double) (this.getM() + this.getQ());
+				this.weights2.put(AAd, (temp + temp2) / temp3);
 			}
 		}
 	}
@@ -406,31 +440,144 @@ public class Family {
 	public void calculateMutualInfo()
 	{
 		double temp;
+		double temp2;
 		double log2 = Math.log(2);
 		
-		for(int i=0; i< getL(); i++)
+		for(int i=0; i< getL()-1; i++)
 		{
-			for(int j=0; j< getL(); j++)
+			for(int j=i+1; j< getL(); j++)
 			{
+				temp2 = 0.0;
 				for(Character a : this.getAATypes())
 				{
 					for(Character b : this.getAATypes())
 					{
 						temp = this.getWeight2(a, i, b, j);
-						temp *= Math.log( temp / ( this.getWeight(a, i) * this.getWeight(b, j) ) ) / log2;
-						this.mutualInfo.put(i+":"+j, temp);
+						temp2 += temp * Math.log( temp / ( this.getWeight(a, i) * this.getWeight(b, j) ) ) / log2;
 					}
 				}
+				this.mutualInfo.put(i+" "+j, temp2);
 			}
 			
 		}
 	}
 	
-	public Double getMutualInfo(int i, int j)
+	public double getMutualInfo(int i, int j)
 	{
-		return this.mutualInfo.get(i+":"+j);
+		Double temp = this.mutualInfo.get(i+" "+j);
+		if(temp == null)
+			return 0.0;
+		return temp;
 	}
 	
+	private HashMap<String,Double> cloneMutualInfo()
+	{
+		HashMap<String,Double> copyMI = new HashMap<String,Double>();
+		
+		for(String key : mutualInfo.keySet())
+		{
+			copyMI.put(key, mutualInfo.get(key));
+		}
+		return copyMI;
+	}
+	
+	public void calculateSortMutualInfo()
+	{
+		double maxV;
+		String maxK = "";
+		Double temp;
+		HashMap<String,Double> copyMI = this.cloneMutualInfo();
+		int max = copyMI.size();
+		
+		for(int i=0; i<max; i++)
+		{
+			maxV = 0.0;
+			for(String key : copyMI.keySet())
+			{
+				temp = copyMI.get(key);
+				if(temp > maxV) {
+					maxV = temp;
+					maxK = key;
+				}
+			}
+			this.miSortValue.add(maxV);
+			this.miSortKey.add(maxK);
+			copyMI.remove(maxK);
+		}
+	}
+	
+	public String displaySortMutualInfo()
+	{
+		String s = "";
+		for(int i=0; i<miSortKey.size(); i++)
+		{
+			s += this.miSortKey.get(i) + "\t" +this.miSortValue.get(i) + "\t" + this.miSortDist.get(i) + "\n";
+		}
+		return s;
+	}
+	
+	
+	/* Distances */
+	
+	public void calculateDistances()
+	{
+		this.distances = FamilyFactory.makeDistances();
+		for(int i=0; i<miSortKey.size(); i++)
+		{
+			this.miSortDist.add(this.distances.get(this.miSortKey.get(i)));
+		}
+	}
+	
+	public String displayDistances()
+	{
+		String s ="";
+		for(String key : distances.keySet())
+		{
+			s += key + " : " + distances.get(key) + "\n"; 
+		}
+		return s;
+	}
+	
+	public double getFraction(int nbPaires)
+	{
+		int nbPaires8 = 0;
+		for(int i=0; i<nbPaires; i++)
+		{
+			if(this.miSortDist.get(i)<8)
+				nbPaires8++;
+		}
+		return nbPaires8 / (double) nbPaires;
+	}
+	
+	/* Fractions */
+	
+	public void calculateFractions()
+	{
+		this.fractions.add(0, 0.0);
+		for(int i=1; i<=50; i++)
+		{
+			this.fractions.add(i, getFraction(i));
+		}
+	}
+	
+	public void generateCurveFractions()
+	{
+		try
+		{
+            String fileName = "./data/Fractions.txt";
+            PrintWriter out  = new PrintWriter(new FileWriter(fileName));
+            
+            for(int i=1; i < fractions.size(); i++)
+            {
+            	out.println(i + " " + fractions.get(i));
+            }
+         
+            out.close();
+	    }
+	    catch(Exception e){
+	      e.printStackTrace();
+	    }
+	}
 	
 	
 	
